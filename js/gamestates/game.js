25f4c62a661a9e51e81e70aa9d97ec97f3488052
Bryan Estrito <bryan.estrito@gmail.com>
2017-06-19 09:43:30 +0800

    Added game states

---
+++ js/gamestates/game.js
@@ -0,0 +1,10 @@
+var game = new Phaser.Game(480, 360, Phaser.AUTO, null, 'game');
+
+//add each game state
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('title', titleState);
+game.state.add('play', playState);
+
+//call the boot state
+game.state.start('boot');
